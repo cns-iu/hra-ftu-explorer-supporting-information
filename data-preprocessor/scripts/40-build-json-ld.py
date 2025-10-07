5ea86d91ea9791b06b58ce0cde15d9f722d48f62
Andreas Bueckle <abueckle@iu.edu>
2025-10-07 13:36:37 -0400

    MVP1 of HRApop in iFTU

--- data-preprocessor/scripts/40-build-json-ld.py
+++ data-preprocessor/scripts/40-build-json-ld.py
@@ -50,7 +50,7 @@ def build_dataset_metadata_jsonld(metadata: pd.DataFrame):
 
     # Fill fields in @graph
     new_ftu["@id"] = (
-        "ttps://lod.humanatlas.io/2d-ftu/prostate-prostate-glandular-acinus"  # hard-coded for now since all datasets with FTUs are from prostate
+        "https://purl.humanatlas.io/2d-ftu/prostate-prostate-glandular-acinus"  # hard-coded for now since all datasets with FTUs are from prostate
     )
 
     for obj in iterate_through_json_lines(
@@ -64,7 +64,7 @@ def build_dataset_metadata_jsonld(metadata: pd.DataFrame):
 
         new_data_source = copy.deepcopy(data_source_instance)
 
-        new_data_source["@id"] = id
+        new_data_source["@id"] = id + "#CellSummary_" + new_ftu["@id"].split("/")[-1]
         new_data_source["label"] = metadata_instance.iloc[0]["handler"]
         new_data_source["link"] = metadata_instance.iloc[0]["dataset_link"]
         new_data_source["description"] = (
@@ -81,13 +81,10 @@ def build_dataset_metadata_jsonld(metadata: pd.DataFrame):
 
     out_json_ld["@graph"] = graph_list
 
-    # Write to file (TEMP)
-    with open(FTU_DATASETS_TEMP, "w", encoding="utf-8") as f:
+    # Write to file
+    print(f"Now saving to {FTU_DATASETS_OUTPUT}")
+    with open(FTU_DATASETS_OUTPUT, "w", encoding="utf-8") as f:
         json.dump(out_json_ld, f, ensure_ascii=False, indent=4)
-
-    # Write to file (OUTPUT)
-    # with open(FTU_DATASETS, "w", encoding="utf-8") as f:
-    #     json.dump(out_json_ld, f, ensure_ascii=False, indent=4)
 
 
 def build_cell_summaries_jsonld():
@@ -99,23 +96,44 @@ def build_cell_summaries_jsonld():
     for obj in iterate_through_json_lines(
         FILTERED_FTU_CELL_TYPE_POPULATIONS_INTERMEDIARY_FILENAME
     ):
+        # Enrich with needed fields
+        obj["cell_source"] = (
+            obj["cell_source"]
+            + "#CellSummary_"
+            + "https://purl.humanatlas.io/2d-ftu/prostate-prostate-glandular-acinus".split(
+                "/"
+            )[
+                -1
+            ]
+        )
+        obj["annotation_method"] = "Aggregation"
+        obj["biomarker_type"] = "gene"
+        obj.pop("modality")
+
+        for summary in obj["summary"]:
+            summary["@type"] = "CellSummaryRow"
+            summary["genes"] = summary.pop("gene_expr")
+            summary["cell_id"] = "http://purl.obolibrary.org/obo/" + summary[
+                "cell_id"
+            ].replace(":", "_")
+
+            for gene in summary["genes"]:
+                gene["@type"] = "GeneExpression"
+                gene["ensemble_id"] = gene.pop("ensembl_id")
+                gene["mean_expression"] = gene.pop("mean_gene_expr_value")
 
         out_json_ld["@graph"].append(obj)
 
-    # Write to file (TEMP)
-    with open(FTU_CELL_SUMMARIES_TEMP, "w", encoding="utf-8") as f:
+    # Write to fil
+    print(f"Now saving to {FTU_CELL_SUMMARIES_OUTPUT}")
+    with open(FTU_CELL_SUMMARIES_OUTPUT, "w", encoding="utf-8") as f:
         json.dump(out_json_ld, f, ensure_ascii=False, indent=4)
-
-    # Write to file (OUTPUT)
-    # with open(FTU_CELL_SUMMARIES, "w", encoding="utf-8") as f:
-    #     json.dump(out_json_ld, f, ensure_ascii=False, indent=4)
 
 
 def main():
     # Driver code
 
     metadata = pd.read_csv(UNIVERSE_METADATA_FILENAME).reset_index(drop=True)
-    pprint(metadata)
 
     build_dataset_metadata_jsonld(metadata=metadata)
     build_cell_summaries_jsonld()
