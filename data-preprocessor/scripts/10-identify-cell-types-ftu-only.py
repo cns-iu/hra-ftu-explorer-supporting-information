cea5cc4c59aa9129acd09721535fbce97a9a0e97
Andreas Bueckle <abueckle@iu.edu>
2026-04-08 14:19:31 -0400

    Updated index.html

--- data-preprocessor/scripts/10-identify-cell-types-ftu-only.py
+++ data-preprocessor/scripts/10-identify-cell-types-ftu-only.py
@@ -1,29 +1,7 @@
 from shared import *
 
 
-def get_organ():
-
-    url = "https://apps.humanatlas.io/api/grlc/hra/2d-ftu-parts.csv"
-
-    headers = {"Accept": "text/csv"}
-
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-
-    ftu_parts = pd.read_csv(StringIO(response.text))
-
-    # print(ftu_parts.head())
-
-    ftu_organ_lookup = ftu_parts[
-        ["ftu_digital_object", "organ_id", "organ_label"]
-    ].drop_duplicates()
-
-    print(ftu_organ_lookup)
-
-    return ftu_organ_lookup
-
-
-def compile_cell_types_per_ftu(query_result: pd.DataFrame, organ_lookup: pd.DataFrame):
+def compile_cell_types_per_ftu(query_result: pd.DataFrame):
 
     result = {}
 
@@ -32,36 +10,12 @@ def compile_cell_types_per_ftu(query_result: pd.DataFrame, organ_lookup: pd.Data
         ftu_label = row.ftu_label
         ct_label = row.ct_label
         ct_iri = iri_to_curie(row.ct_iri)
+        organ_id_short = iri_to_curie(row.organ_iri)
+        organ_label = row.organ_label
 
         # Initialize entry if not exists
         if ftu_label not in result:
-            # print(type(organ_lookup))
-            # print(organ_lookup)
-
-            # organ_data = (
-            #     organ_lookup[["ftu_iri", "organ_id", "organ_label"]]
-            #     .drop_duplicates()
-            #     .set_index("ftu_iri")[["organ_id", "organ_label"]]
-            #     .to_dict(orient="index")
-            # )
-
-            # organ_info = organ_data.get("ftu_iri", {})
-
-            # organ_id = organ_info.get('organ_id')
-            # organ_label = organ_info.get('organ_label')
             print(ftu_purl)
-            matches = organ_lookup.loc[
-                organ_lookup["ftu_digital_object"] == ftu_purl,
-                ["organ_id", "organ_label"],
-            ]
-
-            found = tuple(matches.itertuples(index=False, name=None))
-            print(found)
-
-            if found:
-                organ_id_short, organ_label = found[0]
-            else:
-                organ_id_short, organ_label = None, None
 
             result[ftu_label] = {
                 "ftu_purl": ftu_purl,
@@ -103,7 +57,7 @@ def main():
 
     ftu_query = pd.read_csv(FTU_QUERY)
 
-    result = compile_cell_types_per_ftu(ftu_query, get_organ())
+    result = compile_cell_types_per_ftu(ftu_query)
 
     for item in result:
         pprint(item + " " + str(len(result[item]["cts_exclusive"])))
